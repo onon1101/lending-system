52abf3eae06eb6a533a9e95268c9b0fa2e4a8171
onon1101 <mingzhuguly@gmail.com>
2026-07-30 10:17:37 +0800

    refeator: 移除 top-statement level

--- lending-backend-dotnet/src/LendingSystem.WebApi/Program.cs
+++ lending-backend-dotnet/src/LendingSystem.WebApi/Program.cs
@@ -1,24 +1,34 @@
 using LendingSystem.WebApi.Startup;
+namespace LendingSystem.WebApi;
 
-// Generate the builder
-var builder = WebApplication.CreateBuilder(args);
-
-// config the third party settings
-builder.ConfigureWebHost();
-builder.Services.AddAllModules(
-    builder.Configuration,
-    builder.Environment,
-    typeof(Program).Assembly);
-
-// startup the service 
-var app = builder.Build();
-
-app.UseAllModules();
-
-// finally, run it
-app.Run();
-
-namespace LendingSystem.WebApi
+/// <summary>
+/// Program Class
+/// </summary>
+public sealed class Program
 {
-    public partial class Program;
+
+    /// <summary>
+    /// Program Entry Point
+    /// </summary>
+    /// <param name="args"></param>
+    public static void Main(string[] args)
+    {
+        // Generate the builder
+        var builder = WebApplication.CreateBuilder(args);
+
+        // config the third party settings
+        builder.ConfigureWebHost();
+        builder.Services.AddAllModules(
+            builder.Configuration,
+            builder.Environment,
+            typeof(Program).Assembly);
+
+        // startup the service 
+        var app = builder.Build();
+
+        app.UseAllModules();
+
+        // finally, run it
+        app.Run();
+    }
 }
